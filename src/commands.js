0dec0420502398a63b4993b30ca783d5b21f9e6d
Rafatlne <ashikrafat.lne@gmail.com>
2024-07-15 11:04:19 +0600

    first commit

---
+++ src/commands.js
@@ -0,0 +1,9 @@
+/**
+ * Share command metadata from a common spot to be used for both runtime
+ * and registration.
+ */
+
+export const STRATIV_LAST_SALARY = {
+  name: 'strativ-last-salary',
+  description: 'Strativ Last Salary',
+};
